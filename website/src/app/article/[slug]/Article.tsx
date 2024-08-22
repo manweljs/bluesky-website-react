3285d8ae53083452b46401ff34034f0320db8e63
manweljs <manweljs@gmail.com>
2024-08-22 13:00:25 +0800

    update blog

--- website/src/app/article/[slug]/Article.tsx
+++ website/src/app/article/[slug]/Article.tsx
@@ -7,20 +7,16 @@ import { cls } from '@/utils';
 import dynamic from 'next/dynamic';
 
 const ArticleDetail = dynamic(() => import('bsblog').then(mod => mod.ArticleDetail), { ssr: false });
-const CoreProvider = dynamic(() => import('bsblog').then(mod => mod.CoreProvider), { ssr: false });
 
 export function Article({ slug, privateKey }: { slug: string, privateKey: string }) {
     return (
         <div className={cls(s.blog, s.article_detail)}>
-            <CoreProvider
-                navbarHeight={80}
-            >
-                <ArticleDetail
-                    slug={slug}
-                    privateKey={privateKey}
 
-                />
-            </CoreProvider>
+            <ArticleDetail
+                slug={slug}
+                privateKey={privateKey}
+
+            />
             <Footer />
         </div>
     )
